71b9518b87fef5d7179c5d4c29b2fedf8d5b4d09
LingMan <LingMan@users.noreply.github.com>
2023-12-04 16:29:45 +0100

    Implement 2023 Day 1 Part 1

--- src/year2023/day01.rs
+++ src/year2023/day01.rs
@@ -7,16 +7,29 @@ pub struct Day01<'a> {
 }
 
 impl<'a> Puzzle<'a> for Day01<'a> {
-    type Sol1Type = Option<u64>;
-    type Sol2Type = u64;
+    type Sol1Type = u32;
+    type Sol2Type = Self::Sol1Type;
 
     fn parse(input: &'a str) -> Result<Self> {
-        let lines = input.lines().filter(|line| !line.is_empty()).collect();
+        let lines = input.lines().collect();
         Ok(Self { lines })
     }
 
     fn solve_problem_1(&self) -> Self::Sol1Type {
-        todo!()
+        self.lines.iter()
+            .map(|line| {
+                let mut digits = line.bytes().filter(|c| c.is_ascii_digit());
+
+                let first_digit = digits.next().map(|digit| digit - b'0').unwrap_or_default();
+
+                let last_digit = digits
+                    .next_back()
+                    .map(|digit| digit - b'0')
+                    .unwrap_or(first_digit);
+
+                Self::Sol1Type::from(first_digit * 10 + last_digit)
+            })
+            .sum()
     }
 
     fn solve_problem_2(&self) -> Self::Sol2Type {
